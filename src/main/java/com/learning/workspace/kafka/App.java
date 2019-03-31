44f3ecf4e4b6ac91f25b034d109f440fedca75c5
Ali Imtiaz <ali.imtiaz2010@gmail.com>
2019-03-31 18:43:37 +0500

    Added SimpleProducer, AsynchronousProducer and SynchronousProducer use cases

---
+++ src/main/java/com/learning/workspace/kafka/App.java
@@ -0,0 +1,20 @@
+package com.learning.workspace.kafka;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+/**
+ * 
+ *
+ */
+public class App {
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+	}
+}
